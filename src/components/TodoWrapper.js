559f60c0ff14b45b5fbc081c42c00227dbc5b462
jayammtws <jayamm.tws@tekkiwebsolutions.com>
2024-08-09 20:11:46 +0530

    Update TodoWrapper.js

--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -43,7 +43,7 @@ export const TodoWrapper = () => {
   return (
     <div className="TodoWrapper">
       <h1>Get Your Things Done !</h1>
-      <h2>Todo's</h2>
+      <h1>Todo's</h1>
       <TodoForm addTodo={addTodo} />
       {/* display todos */}
       {todos.map((todo) =>
